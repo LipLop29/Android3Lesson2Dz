2b6588be97eefbce44e4976091159860bf85ce1e
LipLop29 <116729737+LipLop29@users.noreply.github.com>
2023-03-20 17:19:04 +0600

    Update LocationApiService.kt

--- app/src/main/java/com/example/android3lesson2dz/data/network/apiserveces/LocationApiService.kt
+++ app/src/main/java/com/example/android3lesson2dz/data/network/apiserveces/LocationApiService.kt
@@ -1,6 +1,5 @@
 package com.example.android3lesson2dz.data.network.apiserveces
 
-
 import com.example.android3lesson2dz.models.LocationModel
 import com.example.android3lesson2dz.models.RickAndMortyResponse
 import retrofit2.Call
